fcce0a3f36c6bff973bc6c4009ead175915881f9
weissec <44804367+weissec@users.noreply.github.com>
2020-01-15 17:07:50 +0000

    Add files via upload

---
+++ LWBR.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    LWBR (Local Windows Build Reviewer). Retrieve system information and parse them in HTML format for review.
+    See README.md for more information.
+    Author: weissec
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+
+function Enumerate-System {
+
+    "`n[*] Starting System Enumeration.."
+
+    # check if admin or part of admin group
+
+    # 
+
+}
+
+Enumerate-System
